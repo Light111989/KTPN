719735fba3e09bb36837568f3803ff8f47dc0cc8
Light111989 <light.elitetech@gmail.com>
2025-10-09 14:34:28 +0700

    revert npm

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+371dd9d59009cd5fbd5acd148bbeac5371fd4a85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+921508f67703dfd502ce3df1088c8239cecb22e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
